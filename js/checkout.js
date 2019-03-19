cf3a4cd233345ff66df1162c22678acda1d9fe4c
Jesper Wallin <jesper@ifconfig.se>
2019-03-19 16:47:30 +0000

    Add davidshimjs/qrcodejs for generating QR-codes
    Remove the third-party dependency (Google) when generating our
    QR-codes. This is also a better alternative in terms of privacy.

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function($) {
 
+	function wc_gateway_xrp_qrcode(account, tag, amount) {
+		$('#wc_xrp_qrcode').empty();
+		var qrcode = new QRCode(document.getElementById("wc_xrp_qrcode"), {
+			text: "https://ripple.com/send?to=" + account + "&dt=" + tag + "&amount=" + amount,
+			width: 256,
+			height: 256,
+			colorDark : "#000000",
+			colorLight : "#ffffff",
+			correctLevel : QRCode.CorrectLevel.M
+		});
+	}
+
 	var pollTimer = setInterval(function(){
 		jQuery.ajax({
 			"url": ajax_object.ajax_url,
@@ -7,11 +19,14 @@ jQuery(document).ready(function($) {
 			"method": "POST",
 			"dataType": "json",
 			"success": function(data) {
+				var regenqr=false;
 				if ($('#xrp_account').text() != data.xrp_account) {
 					$('#xrp_account').text(data.xrp_account);
+					regenqr=true;
 				}
 				if ($('#destination_tag').text() != data.tag) {
 					$('#destination_tag').text(data.tag);
+					regenqr=true;
 				}
 				if ($('#xrp_total').text() != data.xrp_total) {
 					$('#xrp_total').text(data.xrp_total);
@@ -21,20 +36,25 @@ jQuery(document).ready(function($) {
 				}
 				if ($('#xrp_remaining').text() != data.xrp_remaining) {
 					$('#xrp_remaining').text(data.xrp_remaining);
-				}
-				if ($('#xrp_qr').attr('src') != data.qr) {
-					$('#xrp_qr').attr('src', data.qr);
+					regenqr=true;
 				}
 				if ($('#xrp_status').text() != data.status) {
 					$('#xrp_status').text(data.status);
 				}
 				if (data.raw_status != 'wc-pending') {
 					clearTimeout(pollTimer);
-					$('#xrp_qr').remove();
+					$('#wc_xrp_qrcode').empty();
+				}
+				if (regenqr) {
+					wc_gateway_xrp_qrcode(data.xrp_account, data.tag, data.xrp_remaining);
 				}
 			}
 		});
 	}, 4000);
 
-
+	wc_gateway_xrp_qrcode(
+		$('#xrp_account').text(),
+		$('#destination_tag').text(),
+		$('#xrp_remaining').text()
+	);
 });
