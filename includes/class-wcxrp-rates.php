8af906d99562524373cf52d3cb659ea9b32c03fb
Jens Twesmann <j.twesmann@star-media.biz>
2019-03-28 09:24:02 +0100

    added coinbase

--- includes/class-wcxrp-rates.php
+++ includes/class-wcxrp-rates.php
@@ -401,7 +401,7 @@ class WCXRP_Rates
     }
 
     /**
-     * et Exchange rate from uphold
+     * Get Exchange rate from uphold
      * @return bool|float|int
      */
     private function uphold()
@@ -422,5 +422,30 @@ class WCXRP_Rates
         }
 
         return $this->to_base($rate->ask, $src);
+    }
+
+    /**
+     * Get Exchange rate from coinbase
+     * @return bool|float|int
+     */
+    private function coinbase()
+    {
+        if ($this->base_currency === 'USD') {
+            $url = 'https://api.coinbase.com/v2/prices/XRP-USD/buy';
+            $src = 'USD';
+        } else {
+            $url = 'https://api.coinbase.com/v2/prices/XRP-EUR/buy';
+            $src = 'EUR';
+        }
+        $res = wp_remote_get($url);
+        if (is_wp_error($res) || $res['response']['code'] !== 200) {
+            return false;
+        }
+        if (($rate = json_decode($res['body'])) === null) {
+            return false;
+        }
+
+        return $this->to_base($rate->amount, $src);
+
     }
 }
